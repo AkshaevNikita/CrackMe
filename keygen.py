7a0a17789b817df768f6acadbfea3665dedd1a02
AkshaevNikita <62900379+AkshaevNikita@users.noreply.github.com>
2021-05-11 23:42:02 +0300

    Update keygen.py

--- keygen.py
+++ keygen.py
@@ -7,7 +7,7 @@ def generate_key(name):
     return word ^ 0x5678 ^ 0x1234
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print("Enter a name for registration. (Example: python3 keygen_1.py Alucard)")
+        print("Enter a name for registration. (For example: python3 keygen_my.py Brad)")
         sys.exit(1)
     else:
         print(generate_key(sys.argv[1].upper()))
